c6be08c6534c0325f9afcf5ae39a4632970ee6c2
Zeng Ke <superisaac.ke@gmail.com>
2011-11-12 12:25:35 +0800

    timeline compress/decompress

--- public/js/spec.js
+++ public/js/spec.js
@@ -6,88 +6,85 @@ var timeline_path = {'/statuses/public_timeline': true,
 		     '/search/public_timeline': true
 };
 
-var status_fields = ['id', 'text', 'created_at', 'user'];
-var user_fields = ['id', 'name', 'screen_name', 'profile_image_url', 'friends_count', 'followers_count', 'statuses_count'];
+var status_fields = ['id', 'text', 'created_at',
+		     ['user', 'id'], ['user', 'name'],
+		     ['user', 'screen_name'], ['user', 'profile_image_url'],
+		     ['user', 'friends_count'], ['user', 'followers_count'],
+		     ['user', 'statuses_count']];
 
-function encodeUser(user) {
-    var values = [];
-    for(var i =0; i< user_fields.length; i++) {
-	var v = user[user_fields[i]];
-	if(v == undefined) {
-	    v = null;
-	}
-	values.push(v);
-    }
-    return values;
-}
+//var user_fields = ['id', 'name', 'screen_name', 'profile_image_url', 'friends_count', 'followers_count', 'statuses_count'];
 
-function decodeUser(val) {
-    if(val == undefined) {
-	console.info('undefined');
-	return null;
-    }
-    var start = 0;
-    var user = new Object();
-    for(var i=0; i<user_fields.length; i++) {
-	var key = user_fields[i];
-	var v = val[i];
-	user[key] = v;
-    }
-    return user;
-}
-
-function encodeStatus(values, status) {
-    var len = status_fields.length;
-    for(var i = 0; i<len; i++) {
-	var key = status_fields[i];
-	var v;
-	if(key == 'user') {
-	    v = encodeUser(status[key]);
-	} else if (key == 'created_at') {
-	    v = status[key];
-	    v = new Date(v).getTime();
-	    v = v / 1000;
-	} else {
-	    v = status[key];
-	}
-	if(v == undefined) {
-	    v = null;
-	}
-	values[key].push(v);
-    }
-}
-
-function decodeStatus(values, index) {
-    var status = new Object();
-    for(var i=0; i<status_fields.length; i++) {
-	var key = status_fields[i];
-	var v = values[key][index];
-	if(key == 'user') {
-	    v = decodeUser(v);
-	    if(!v) {
-		console.info(values[key]);
+function decodeObject(fields, values, index) {
+    var len = fields.length;
+    var obj = new Object();
+    for(var i=0; i<len; i++) {
+	var key = fields[i];
+	if(typeof key == 'string') {
+	    obj[key] = values[i][index];
+	} else { // Array
+	    var m = obj;
+	    var j = 0;
+	    for(; j<key.length - 1; j++) {
+		var subkey = key[j];
+		if(m[subkey] == undefined) {
+		    m[subkey] = new Object();
+		}
+		m = m[subkey];
 	    }
-	} else if(key == 'created_at') {
-	    v = new Date(v * 1000);
+	    m[key[j]] = values[i][index];
 	}
-	status[key] = v;
     }
-    return status;
+    return obj;
+}
+
+function encodeObject(fields, values, obj) {
+    var len = fields.length;
+    for(var i =0; i<len; i++) {
+	var key = fields[i];
+	var v;
+	if(typeof key == 'string') {
+	    v = obj[key];
+	} else { //Array
+	    v = obj;
+	    for(var j = 0; j<key.length; j++) {
+		v = v[key[j]];
+		if(v == undefined) {
+		    break;
+		}
+	    }
+	}
+	if(v == undefined) {
+	    v = null;
+	}
+	values[i].push(v);	
+    }
 }
 
 function encodeTimeline(path, timeline) {
     if(timeline_path[path]) {
-	var values = {};
+	var values = [];
+	var created_at_index = -1;
 	for(var i=0; i<status_fields.length; i++) {
-	    values[status_fields[i]] = [];
+	    values[i] = new Array();
+	    if(status_fields[i] == 'created_at'){
+		created_at_index = i;
+	    }	
 	}
+
 	for(var i=0; i<timeline.length; i++) {
 	    var status = timeline[i];
-	    var st = encodeStatus(values, status);
+	    encodeObject(status_fields, values, status);
 	}
-	var newvalues = [];
-	for(var i=0; i<status_fields.length; i++) {
-	    newvalues.push(values[status_fields[i]]);
+
+	if(created_at_index >= 0) {
+	    var narr = new Array();
+	    for(var i=0; i<values[created_at_index].length; i++) {
+		var ts = values[created_at_index][i];
+		var v = new Date(ts).getTime();
+		//v /= 1000;
+		narr.push(v);
+	    }
+	    values[created_at_index] = narr;
 	}
 	return values;
     } else {
@@ -96,10 +93,10 @@ function encodeTimeline(path, timeline) {
 }
 
 function decodeTimeline(values) {
-    var elem_len = values[status_fields[0]].length;
+    var elem_len = values[0].length;
     var timeline = [];
     for(var i=0; i<elem_len; i++) {
-	var status = decodeStatus(values, i);
+	var status = decodeObject(status_fields, values, i);
 	timeline.push(status);
     }
     return timeline;
